8e8326a6dbab65d40c797905c27ffeaf1b4db993
mauricio <111543689+MMTURC@users.noreply.github.com>
2025-11-27 23:02:28 -0500

    Solving the Seven Bros

---
+++ learn/fix.py
@@ -0,0 +1,11 @@
+# Fix the Code: Seven Brothers
+# Print the names of the brothers so they are alphabetical
+
+
+print("Aapo")
+print("Eero")
+print("Juhani")
+print("Lauri")
+print("Simeoni")
+print("Timo")
+print("Tuomas")
