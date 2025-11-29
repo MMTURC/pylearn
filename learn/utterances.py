45c877de938412ea6073593edf0d0eb950695784
mauricio <111543689+MMTURC@users.noreply.github.com>
2025-11-29 12:26:45 -0500

    utterances excercise done!

---
+++ learn/utterances.py
@@ -0,0 +1,10 @@
+# We should ask for three utterances and print them out
+
+
+part_one = input("The first part: ")
+
+part_two = input("The second part: ")
+
+part_three = input("The third part: ")
+
+print(part_one + "-" + part_two + "-" + part_three + "!")
