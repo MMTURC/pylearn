feee8028b7c8fc508798ab426a0d6c091db73056
mauricio <111543689+MMTURC@users.noreply.github.com>
2025-11-29 12:41:29 -0500

    Solved the story it took way too long to figure out I was failing the test because of a typo

---
+++ learn/story.py
@@ -0,0 +1,10 @@
+name = input("Please type in a name: ")
+year = input("Please type in a year: ")
+
+print(
+    name + " is a valient knight, born in the year " + year + ". One morning " + name + " woke up to an " + "awful racket: a dragon was approaching the villiage. Only " + name + " could save the village's residents."
+)
+#
+# print(
+  #   "awful racket: a dragon was approaching the villiage. Only " + name + " could save the village's residents."
+ # )
