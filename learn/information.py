220aeafce1177c78900a2d1e51725776b30d4c09
mauricio <111543689+MMTURC@users.noreply.github.com>
2025-11-28 19:34:50 -0500

    Solved excercise where you need users address, last_name, city

---
+++ learn/information.py
@@ -0,0 +1,13 @@
+# Please write a program which asks for the user's name and address
+
+
+name = input("Given name: ")
+family_name = input("Family name: ")
+address = input("Street address: ")
+city_zipcode = input("City and postal code: ")
+
+print(name + " " + family_name)
+
+print(address)
+
+print(city_zipcode)
