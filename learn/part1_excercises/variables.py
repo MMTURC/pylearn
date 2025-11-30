6ef997cf845abc8c3bda7f5caba7c1645c81cf73
mauricio <111543689+MMTURC@users.noreply.github.com>
2025-11-30 11:18:14 -0500

    Created a part 1 excercises and changed several files

---
+++ learn/part1_excercises/variables.py
@@ -0,0 +1,25 @@
+
+
+
+result = 10 * 16
+
+print("Result is " + (str(result)))
+
+# You can also write it like this
+
+
+print("Result is " , result)
+
+
+# My favorite way to write it!
+
+
+print(f"result is {result}")
+
+name = "dan"
+
+age = 45
+
+city = "America"
+
+print(f"hi {name} your age is {age} and the city you are from {city}")
