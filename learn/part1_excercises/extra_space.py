bd4497db4942d707611b87d877b8c01034d87bc3
mauricio <111543689+MMTURC@users.noreply.github.com>
2025-11-30 12:15:57 -0500

    Check

---
+++ learn/part1_excercises/extra_space.py
@@ -0,0 +1,13 @@
+# Your friend is working on an appy for jobseekers. She sends you this big of code:
+
+name = "Tim Tester"
+age = 20
+skill_one = "python"
+level_one = "beginner"
+skill_two = "java"
+level_two = "veteran"
+skill_three = "programming"
+level_three = "semiprofessional"
+
+lower = 2000
+upper = 3000
