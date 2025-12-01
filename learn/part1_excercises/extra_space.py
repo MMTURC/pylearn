1f5e4cb5a3ed67afcec788fe44918fae76d8a09b
mauricio <111543689+MMTURC@users.noreply.github.com>
2025-11-30 19:47:17 -0500

    I guess the other commit didn't work added comments for clarifications

--- learn/part1_excercises/extra_space.py
+++ learn/part1_excercises/extra_space.py
@@ -8,16 +8,19 @@ skill_two = "java"
 level_two = "veteran"
 skill_three = "programming"
 level_three = "semiprofessional"
-
 lower = 2000
 upper = 3000
 
 print(f"My name is {name}, I am {age} years old")
-
-print(f"my skills are")
 print("")
+print(f"my skills are")
 print(f"- {skill_one} ({level_one})")
 print(f"- {skill_two} ({level_two})")
 print(f"- {skill_three} ({level_three}")
 print(" ")
 print(f"I am looking for a job with a salary of {lower}-{upper} euros per month")
+
+
+# The test cases for this particular excercise are pretty strict so you might need to play around with it
+# in order to get the completion
+# I am too lazy to fix it exactly how the thing wanted it.
